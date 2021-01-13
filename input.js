82272e129a75f3ff2aaf64183faf096a0103f2a4
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-13 17:59:43 -0500

    replaced logs for connection.write and declared connection variable

--- input.js
+++ input.js
@@ -2,13 +2,24 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
+let connection;
+
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  } else if(key === 'w'){
+    connection.write("Move: up");
+  } else if(key === 'a') {
+    connection.write('Move: left')
+  } else if(key === 's'){
+    connection.write('Move: down')
+  } else if(key === 'd'){
+    connection.write('Move: right')
   }
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.on('data', handleUserInput)
   stdin.setRawMode(true);
