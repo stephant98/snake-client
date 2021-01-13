d656582565c1dba669b6668bbe49533b72413e76
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-13 14:06:32 -0500

    refactored file to just require setupInput function

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input')
 
 console.log('Connecting ...');
 connect();
 
-/*
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.on('data', handleUserInput)
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
 
 setupInput();
