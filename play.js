cf1d584c42fd22aeea038a786cec76bffc01d150
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-13 12:59:46 -0500

    refactored play.js to require function from client.js

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-  
-  
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
