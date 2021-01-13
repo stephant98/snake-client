5eb8c018d00d8de427099dd9f975d297bef3e2a9
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-13 12:47:16 -0500

    added event handler to handle incoming data

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+  
+  
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
